a266906d650d3bf0e4872e77e0e1c085bb7930f0
Robinson <rlrobinson.2017@mymail.jcsu.edu>
2018-02-08 12:37:37 -0500

    first commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+
+public class Card {
+	int Number;
+	String Suit;
+
+	public Card(int Number1,String Suit1) 
+	{
+		Number=Number1;
+		Suit=Suit1;
+	}
+
+}
---
+++ src/CardTest.java
@@ -0,0 +1,22 @@
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Card card1Type1=new Card(3,"Hearts");
+		Card card2Type1=new Card(2,"Hearts");
+		Card card3Type1=new Card(8,"Hearts");
+		Card card1Type2=new Card(9,"Diamonds");
+		Card card2Type2=new Card(7,"Diamonds");
+		Card card3Type2=new Card(2,"Diamonds");
+		Card card1Type3=new Card(8,"Spades");
+		Card card2Type3=new Card(3,"Spades");
+		Card Lucky=new Card(2,"Spades");
+		System.out.println(card1Type1.Suit);
+		System.out.println(card1Type2.Suit);
+		System.out.println(card1Type3.Suit);
+		
+		
+	}
+
+}
